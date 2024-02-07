79ba1934726e539f98894f9df7d1424988a9cc87
ChristianSantanaLlanes <82761124+ChristianSantanaLlanes@users.noreply.github.com>
2024-02-06 21:07:03 -0600

    ....

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,49 @@
 // @ts-ignore
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, reference, z } from 'astro:content';
 
-const posts = defineCollection({
-  // Type-check frontmatter using a schema
+const games = defineCollection({
+  type: 'data',
   schema: z.object({
     title: z.string(),
-  }),
-});
+    poster: z.string().optional(),
+    trailer: z.string().optional(),
+    description: z.string().optional(),
+    caracteristicas: z.array(reference('caracteristicas')).optional(),
+    plataformas: z.array(reference('plataformas')).optional(),
+    generos: z.array(reference('generos')).optional(),
+    vistas: z.array(reference('vistas')).optional(),
+    companias: z.array(reference('companias')).optional(),
+  })
+})
+const caracteristicas = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string()
+  })
+})
+const plataformas = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string()
+  })
+})
+const generos = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string()
+  })
+})
+const vistas = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string()
+  })
+})
+const companias = defineCollection({
+  type: 'data',
+  schema: z.object({
+    title: z.string()
+  })
+})
 
-export const collections = { posts };
+export const collections = { games, caracteristicas, plataformas, generos, vistas, companias };
